35958166556d406fc13664e9edfb2e6aa182072d
hllry2022 <119042763+hllry2022@users.noreply.github.com>
2025-02-11 17:33:10 +0300

    Add files via upload

---
+++ my_app.py
@@ -0,0 +1,13 @@
+# Streamlit Documentation: https://docs.streamlit.io/
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+from PIL import Image  # to deal with images (PIL: Python imaging library)
+
+# TEXT ELEMENTS
+# Title/Text
+st.title("This is a title")
+st.text("This is some test.")
+
